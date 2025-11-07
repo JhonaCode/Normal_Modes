fc236787ad1f0a8fbb3dc22c1fe5f6f91a5903e7
jhonatan <jhonatanjaam@gmail.com>
2025-11-07 10:04:58 -0300

    x

--- Version_4.0/python_ss/ss_epf.py
+++ Version_4.0/python_ss/ss_epf.py
@@ -9,9 +9,51 @@ import normal_modes.epflux as tv
 
 args={
     "caso": "ERA_5",
+    "epoca": 2,
+    "csst": "ClimSS",
+    "bcolor": [-8.0,10,10],
+    "lats":  [-85,-25,5],
+    "color": 'jet',    
+    "scale":5, 
+    "width":0.0050, 
+    "magwind":10.0,
+    "show" : True,
+    "fmt":'.0f', 
+    "units":'m/s', 
+}
+
+arg=tv.epf(args)
+
+#exit()
+
+lats=  [-70,-10,5]
+#To load the appropiated file given the args. 
+
+args={
+    "caso": "ERA_5",
     "epoca": 37,
     #"csst": "ClimRS",
     "csst": "RainSS",
+    "bcolor": [-8.0,10,10],
+    "lats":  lats, 
+    "color": 'jet',    
+    "scale":5, 
+    "width":0.0050, 
+    "magwind":10.0,
+    "show" : True,
+    "fmt":'.0f', 
+    "units":'m/s', 
+}
+
+#To load the appropiated file given the args. 
+arg=tv.epf(args)
+
+args={
+    "caso": "ERA_5",
+    "epoca": 53,
+    #"csst": "ClimRS",
+    "csst": "RainSS",
+    "lats":  lats, 
     "bcolor": [-8.0,10,10],
     "color": 'jet',    
     "scale":5, 
@@ -25,25 +67,41 @@ args={
 #To load the appropiated file given the args. 
 arg=tv.epf(args)
 
-exit()
 
 args={
     "caso": "BAMHY",
     "epoca": 10,
     #"csst": "ClimRS",
     "csst": "RainSS",
-    "bcolor": [-140.0,160,15],
-    "lats":[-90,90,6], 
-    "lons":[0,360,6], 
+    "bcolor": [-8.0,10,10],
+    "lats":  lats, 
     "color": 'jet',    
-    "scale":300, 
-    "width":0.0020, 
-    "magwind":20.0,
+    "scale":5, 
+    "width":0.0050, 
+    "magwind":10.0,
     "show" : True,
     "fmt":'.0f', 
     "units":'m/s', 
 }
 
 #To load the appropiated file given the args. 
-#arg=tv.anomrc(args)
+arg=tv.epf(args)
 
+args={
+    "caso": "BAMHY",
+    "epoca": 14,
+    #"csst": "ClimRS",
+    "csst": "RainSS",
+    "lats":  lats, 
+    "bcolor": [-8.0,10,10],
+    "color": 'jet',    
+    "scale":5, 
+    "width":0.0050, 
+    "magwind":10.0,
+    "show" : True,
+    "fmt":'.0f', 
+    "units":'m/s', 
+}
+
+#To load the appropiated file given the args. 
+arg=tv.epf(args)
